1bf83ce9367842ff5226eddb3f5cabc020f2c620
DraganPortfolioDevelop <87099063+DraganPortfolioDevelop@users.noreply.github.com>
2026-02-17 22:49:38 +0100

    Update main-v1.js

--- main-v1.js
+++ main-v1.js
@@ -260,52 +260,6 @@ $(document).ready(function() {
     }, 10000); // 10 second fallback
 });
 
-// Poveži leave_review.html sa Google backendom
-const backendUrl = "https://script.google.com/macros/s/AKfycbygkJtJwE5wOsV5tk_Gg8x66qVrxUN5LvRd3Rc0IcJC2yq6ZjKjA1GMCKvnNiFDO_AXCw/exec";
-
-$("#sendReview").submit(function(e) {
-  e.preventDefault();
-
-  $.ajax({
-    url: backendUrl,
-    method: "POST",
-    contentType: "application/json",
-    data: JSON.stringify({
-      name: $("#name").val(),
-      email: $("#email").val(),
-      rating: $("#rating").val(),
-      comment: $("#comment").val()
-    }),
-    success: function() {
-      alert("Hvala vam! Vaša recenzija je uspešno poslata.");
-    },
-    error: function(err) {
-      console.log("Greška:", err);
-      alert("Došlo je do greške. Pokušajte ponovo.");
-    }
-  });
-});
-// Kako da prikažeš recenzije na sajtu (index.html)
-fetch("https://docs.google.com/spreadsheets/d/1pDeA7dVekYPuF2V_jrGyg0qYj20u5bqTTA9JMsaCUNw/gviz/tq?tqx=out:json")
-  .then(r => r.text())
-  .then(text => {
-    const json = JSON.parse(text.substring(47, text.length - 2));
-    const rows = json.table.rows;
-
-    rows.forEach(row => {
-      const name = row.c[0]?.v;
-      const email = row.c[1]?.v;
-      const rating = row.c[2]?.v;
-      const comment = row.c[3]?.v;
-      const approved = row.c[4]?.v;
-
-      if (approved === "yes") {
-        document.querySelector("#reviews").innerHTML += ` <div class="review"> <h3>${name}</h3> <p>${comment}</p> <p>${rating} ⭐</p> </div> `;
-      }
-    });
-  });
-
-
 // ------------------ Pick up from Instagram top 6 posts for gallery
 // $(document).ready(function() {
 //   var profileId = "your-instagram-profile-id";
@@ -330,6 +284,7 @@ fetch("https://docs.google.com/spreadsheets/d/1pDeA7dVekYPuF2V_jrGyg0qYj20u5bqTT
 //     }
 //   });
 // });
+
 
 
 
