e956bb6e6b77d1bdea875e7a4a6b8d883cc9bff6
DraganPortfolioDevelop <87099063+DraganPortfolioDevelop@users.noreply.github.com>
2026-02-17 23:07:10 +0100

    Update review.js

--- review.js
+++ review.js
@@ -1,18 +1,16 @@
-const backendUrl = "https://script.google.com/macros/s/AKfycbygkJtJwE5wOsV5tk_Gg8x66qVrxUN5LvRd3Rc0IcJC2yq6ZjKjA1GMCKvnNiFDO_AXCw/exec";
-
 $("#sendReview").submit(function(e) {
   e.preventDefault();
 
   $.ajax({
     url: backendUrl,
     method: "POST",
-    contentType: "application/json",
-    data: JSON.stringify({
+    contentType: "application/x-www-form-urlencoded",
+    data: {
       name: $("#name").val(),
       email: $("#email").val(),
       rating: $("#rating").val(),
       comment: $("#comment").val()
-    }),
+    },
     success: function() {
       alert("Hvala vam! Vaša recenzija je uspešno poslata.");
     },
