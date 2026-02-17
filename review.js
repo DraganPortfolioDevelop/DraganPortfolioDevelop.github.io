89f6f493a352929e38f9d328fc927f5a7d3f7bf9
DraganPortfolioDevelop <87099063+DraganPortfolioDevelop@users.noreply.github.com>
2026-02-17 23:09:19 +0100

    Update review.js

--- review.js
+++ review.js
@@ -1,3 +1,5 @@
+const backendUrl = "https://script.google.com/macros/s/AKfycbygkJtJwE5wOsV5tk_Gg8x66qVrxUN5LvRd3Rc0IcJC2yq6ZjKjA1GMCKvnNiFDO_AXCw/exec";
+
 $("#sendReview").submit(function(e) {
   e.preventDefault();
 
@@ -20,3 +22,4 @@ $("#sendReview").submit(function(e) {
     }
   });
 });
+
