e02493f2dcd7438ffdfc3711d0d4c7ecf94358b1
DraganPortfolioDevelop <87099063+DraganPortfolioDevelop@users.noreply.github.com>
2026-02-17 22:28:57 +0100

    Add files via upload

---
+++ review.js
@@ -0,0 +1,24 @@
+const backendUrl = "https://script.google.com/macros/s/AKfycbygkJtJwE5wOsV5tk_Gg8x66qVrxUN5LvRd3Rc0IcJC2yq6ZjKjA1GMCKvnNiFDO_AXCw/exec";
+
+$("#sendReview").submit(function(e) {
+  e.preventDefault();
+
+  $.ajax({
+    url: backendUrl,
+    method: "POST",
+    contentType: "application/json",
+    data: JSON.stringify({
+      name: $("#name").val(),
+      email: $("#email").val(),
+      rating: $("#rating").val(),
+      comment: $("#comment").val()
+    }),
+    success: function() {
+      alert("Hvala vam! Vaša recenzija je uspešno poslata.");
+    },
+    error: function(err) {
+      console.log("Greška:", err);
+      alert("Došlo je do greške. Pokušajte ponovo.");
+    }
+  });
+});
